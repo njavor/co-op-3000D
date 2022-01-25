7a10c2718ab996f22b755bdb9deaf50590abe485
Niki Jávor <nikolettjavor@gmail.com>
2022-01-25 10:35:59 +0100

    feladatok

---
+++ main.py
@@ -0,0 +1,15 @@
+"""
+Olvassuk be az alábbi fájl tartalmmát egy listába/tömbbe,
+majd a következő feladatokat oldjuk meg.
+Minden feladat előtt a program írja ki a feladat sorszámát!
+1. Mennyi a sorozatban található számok szorzata?
+2. Írjuk ki az utolsó 5-tel vagy 7-tel osztható szám indexét!
+3. Írjuk ki az első 3-mal és 7-tel osztható szám indexét!
+4. Igaz-e, hogy minden szám negatív?
+5. Van-e a sorozatban olyan szám, amelyik 1 és 10 közé esik?
+6. Hány 18-cal osztható szám található a sorozatban?
+7. Mennyi a sorozatban található egyik legkisebb szám indexe?
+8. Írjuk ki a sorozatban található 17-tel vagy 18-cal osztható számok négyzetét!
+9. Hány eleme van a sorozatnak?
+10. Van-e a sorozatban olyan negatív szám, amelynek az összes szomszédja pozitív?
+"""
