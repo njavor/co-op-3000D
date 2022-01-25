eb106a93bc71235a19b74a6fecea39a3f778f72e
Niki Jávor <nikolettjavor@gmail.com>
2022-01-25 10:46:56 +0100

    main

--- main.py
+++ main.py
@@ -14,7 +14,39 @@ Minden feladat előtt a program írja ki a feladat sorszámát!
 10. Van-e a sorozatban olyan negatív szám, amelynek az összes szomszédja pozitív?
 """
 
+from első import egy
+from második import kettő
+from harmadik import három
+from negyedik import négy
+from ötödik import öt
+from hatodik import hat
+from hetedik import hét
+from nyolcadik import nyolc
+from kilencedik import kilenc
+from tizedik import tíz
+
 lista = []
 with open("input.txt","r",encoding="utf8") as f:
     for sor in f:
         lista.append(int(lista))
+
+print("1. Van-e a sorozatban 100-zal osztható szám?")
+egy(lista)
+print("2. Írjuk ki az utolsó 7-tel osztható szám indexét!")
+kettő(lista)
+print("3. Írjuk ki az első 19-cel osztható szám indexét!")
+három(lista)
+print("4. Mennyi a sorozatban található számok átlagának a négyzete?")
+négy(lista)
+print("5. Igaz-e, hogy minden szám nagyobb, mint 10?")
+öt(lista)
+print("6. Hány 9-cel osztható szám található a sorozatban?")
+hat(lista)
+print("7. Írjuk ki a sorozatban található 15-tel osztható számok felét!")
+hét(lista)
+print("8. Hány eleme van a sorozatnak?")
+nyolc(lista)
+print("9. Van-e a sorozatban olyan negatív szám, amelyet pozitív követ?")
+kilenc(lista)
+print("10. Mennyi a sorozatban található legkisebb szám fele?")
+tíz(lista)
